b7cf24f3b146905b568088729a6feee8bba2df81
MisterioDev <pauldelacruzcancho@gmail.com>
2023-05-13 20:21:57 -0500

    Agregando contenido - Formularios

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import scrollTopButton from './btn_scroll.js';
+import contactFormValidations from './validaciones_formularios.js';
 
 const d = document;
 
 d.addEventListener('DOMContentLoaded', (e) => {
   scrollTopButton('.scroll-top-btn');
+  contactFormValidations();
 });
